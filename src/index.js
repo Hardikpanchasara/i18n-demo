db8fd6ef0cb2520f97758b870b1329d9d4e749d1
Hardik Panchasara <118798473+Hardikpanchasara@users.noreply.github.com>
2023-07-22 19:53:27 +0530

    initialize project

---
+++ src/index.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import tEn from "./locales/en/transaltion.json"
+import tFr from "./locales/fr/transaltion.json"
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: tEn
+      },
+      fr: {
+        translation: tFr
+      }
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
+
+
+const ChangeLang = (l) => {
+  return () => {
+    // alert("language changed in " + l)
+    i18n.changeLanguage(l)
+    localStorage.setItem("lang", l)
+  }
+
+}
+
+
+function App() {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    // alert("page loaded")
+    const CurrentLanguage = localStorage.getItem("lang")
+    i18n.changeLanguage(CurrentLanguage)
+
+  }, [])
+
+
+
+  return (
+    <>
+      <h1>{t('test_2')}</h1>
+
+      <button onClick={ChangeLang("en")}>english</button>
+      <button onClick={ChangeLang("fr")}>french</button>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
